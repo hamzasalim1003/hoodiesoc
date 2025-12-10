3fa41ff0934543bcf50da25d399414079618719e
hamzasalim1003 <hamzasalim1003@gmail.com>
2025-12-10 12:04:30 +0000

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero';
 import Highlights from '../components/Highlights';
 import Collections from '../components/Collections';
 import SchoolLeaversPromo from '../components/SchoolLeaversPromo';
-import CauseSection from '../components/CauseSection';
 import TestimonialSlider from '../components/TestimonialSlider';
 import TrustBadges from '../components/TrustBadges';
 
@@ -14,7 +13,6 @@ const Home = () => {
       <Highlights />
       <Collections />
       <SchoolLeaversPromo />
-      <CauseSection />
       <TestimonialSlider />
       <TrustBadges />
     </div>
